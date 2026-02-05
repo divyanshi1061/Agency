cb364402307e5b8166c4aa42596418c556358538
dictalk <divyanshikhandelwal755@gmail.com>
2026-02-05 23:20:24 +0530

    commit

--- src/components/OurWork.jsx
+++ src/components/OurWork.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Title from './Title'
 
 const OurWork = () => {
   return (
-    <div id=""></div>
+    <div id="our-work" className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
+        <Title title={'Our Work'} desc={'Discover how we have helped businesses like yours achieve their goals through innovative digital solutions.'}/>
+    </div>
   )
 }
 
